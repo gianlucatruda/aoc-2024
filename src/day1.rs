4f11e0cdb364f5975b654e79151ee1b2a29ace86
Gianluca Truda <dev@gianluca.ai>
2024-12-02 19:03:11 +0000

    Unit tests on example for day1.

--- src/day1.rs
+++ src/day1.rs
@@ -2,8 +2,7 @@
 
 use std::{fs, iter::zip};
 
-fn parse_and_sort_lists() -> (Vec<u32>, Vec<u32>) {
-    let input = fs::read_to_string("data/day1.txt").expect("Read input");
+fn parse_and_sort_lists(input: String) -> (Vec<u32>, Vec<u32>) {
     let mut a: Vec<u32> = Vec::new();
     let mut b: Vec<u32> = Vec::new();
 
@@ -25,7 +24,7 @@ fn parse_and_sort_lists() -> (Vec<u32>, Vec<u32>) {
     (a, b)
 }
 
-pub fn part1(a: &Vec<u32>, b: &Vec<u32>) {
+pub fn part1(a: &Vec<u32>, b: &Vec<u32>) -> u32 {
     let mut total: u32 = 0;
     for (l, r) in zip(a, b) {
         let val = match l >= r {
@@ -35,9 +34,10 @@ pub fn part1(a: &Vec<u32>, b: &Vec<u32>) {
         total += val;
     }
     println!("Day 1 part 1: {total}");
+    return total;
 }
 
-pub fn part2(a: &Vec<u32>, b: &Vec<u32>) {
+pub fn part2(a: &Vec<u32>, b: &Vec<u32>) -> u32 {
     let mut score: u32 = 0;
     for av in a.iter() {
         let mut hits = 0;
@@ -49,10 +49,31 @@ pub fn part2(a: &Vec<u32>, b: &Vec<u32>) {
         score += av * hits;
     }
     println!("Day 1 part 2: {score}");
+    return score;
 }
 
 pub fn run() {
-    let (a, b) = parse_and_sort_lists();
+    let input = fs::read_to_string("data/day1.txt").expect("Read input");
+    let (a, b) = parse_and_sort_lists(input);
     part1(&a, &b);
     part2(&a, &b);
+}
+
+const TESTCASE: &str = "\
+3   4
+4   3
+2   5
+1   3
+3   9
+3   3";
+
+#[test]
+fn p1() {
+    let (a, b) = parse_and_sort_lists(TESTCASE.to_string());
+    assert_eq!(part1(&a, &b), 11);
+}
+#[test]
+fn p2() {
+    let (a, b) = parse_and_sort_lists(TESTCASE.to_string());
+    assert_eq!(part2(&a, &b), 31);
 }
