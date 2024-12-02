e7403a7ed51b3d662e801b9683245595733357a1
Gianluca Truda <dev@gianluca.ai>
2024-12-02 18:09:10 +0000

    Modularised day 1

--- README.md
+++ README.md
@@ -6,9 +6,12 @@ Disclaimer: I [started learning Rust](https://gianluca.ai/tags/rust/) a few mont
 
 ## Progress log
 
-- [ ] Day 1
+- [x] Day 1
+    - [x] Part 1
+    - [x] Part 2
+    - [ ] Optimised
 - [ ] Day 2
 
 ## Notes
 
-
+I'm explicitly telling `main.rs` which other filenames (e.g. `day1.rs`) to consider modules and import (e.g. `mod day1;`) and then calling the public `run()` function for each, which prints the answers using `input.txt` (which is different every day and unique per user).
---
+++ src/day1.rs
@@ -0,0 +1,58 @@
+// https://adventofcode.com/2024/day/1
+
+use std::{fs, iter::zip};
+
+fn parse_and_sort_lists() -> (Vec<u32>, Vec<u32>) {
+    let input = fs::read_to_string("input.txt").expect("Read input");
+    let mut a: Vec<u32> = Vec::new();
+    let mut b: Vec<u32> = Vec::new();
+
+    for line in input.lines() {
+        let data: Vec<&str> = line.split_whitespace().collect();
+        if data.len() < 2 {
+            continue;
+        }
+        let left: u32 = data[0].parse().expect("str to int");
+        let right: u32 = data[1].parse().expect("str to int");
+
+        a.push(left);
+        b.push(right);
+    }
+
+    a.sort();
+    b.sort();
+
+    (a, b)
+}
+
+pub fn part1(a: &Vec<u32>, b: &Vec<u32>) {
+    let mut total: u32 = 0;
+    for (l, r) in zip(a, b) {
+        let val = match l >= r {
+            true => l - r,
+            _ => r - l,
+        };
+        total += val;
+    }
+    println!("Day 1 part 1: {total}");
+}
+
+pub fn part2(a: &Vec<u32>, b: &Vec<u32>) {
+    let mut score: u32 = 0;
+    for av in a.iter() {
+        let mut hits = 0;
+        for bv in b.iter() {
+            if av == bv {
+                hits += 1;
+            }
+        }
+        score += av * hits;
+    }
+    println!("Day 1 part 2: {score}");
+}
+
+pub fn run() {
+    let (a, b) = parse_and_sort_lists();
+    part1(&a, &b);
+    part2(&a, &b);
+}
