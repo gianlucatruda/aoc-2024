d257849ec9b53fb1282eb9e84c1e7b4c13273f88
Gianluca Truda <dev@gianluca.ai>
2024-12-02 17:50:45 +0000

    Day 1b. Scrappy solution.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::{fs, iter::zip};
 
-fn main() {
+fn parse_and_sort_lists() -> (Vec<u32>, Vec<u32>) {
     let input = fs::read_to_string("input.txt").expect("Read input");
     let mut a: Vec<u32> = Vec::new();
     let mut b: Vec<u32> = Vec::new();
@@ -25,15 +25,38 @@ fn main() {
     // println!("A: {a:?}");
     // println!("B: {b:?}");
 
+    (a, b)
+}
+
+fn part1(a: &Vec<u32>, b: &Vec<u32>) {
     let mut total: u32 = 0;
     for (l, r) in zip(a, b) {
         let val = match l >= r {
             true => l - r,
             _ => r - l,
         };
-        total += val;
-
         // println!("{val}");
+        total += val;
     }
-    println!("Answer: {total}");
+    println!("Day 1 part 1: {total}");
+}
+
+fn part2(a: &Vec<u32>, b: &Vec<u32>) {
+    let mut score: u32 = 0;
+    for av in a.iter() {
+        let mut hits = 0;
+        for bv in b.iter() {
+            if av == bv {
+                hits += 1;
+            }
+        }
+        score += av * hits;
+    }
+    println!("Day 1 part 2: {score}");
+}
+
+fn main() {
+    let (a, b) = parse_and_sort_lists();
+    part1(&a, &b);
+    part2(&a, &b);
 }
