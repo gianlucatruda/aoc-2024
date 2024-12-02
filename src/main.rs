dda0fbc6e4164b47e864696379d51bfac76d4cd0
Gianluca Truda <dev@gianluca.ai>
2024-12-02 17:56:33 +0000

    Tidying up

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,12 @@ fn parse_and_sort_lists() -> (Vec<u32>, Vec<u32>) {
     let mut b: Vec<u32> = Vec::new();
 
     for line in input.lines() {
-        // println!("{line}");
         let data: Vec<&str> = line.split_whitespace().collect();
         if data.len() < 2 {
             continue;
         }
-        // println!("{data:?}");
         let left: u32 = data[0].parse().expect("str to int");
         let right: u32 = data[1].parse().expect("str to int");
-        // println!("{left} vs {right}");
 
         a.push(left);
         b.push(right);
@@ -22,8 +19,6 @@ fn parse_and_sort_lists() -> (Vec<u32>, Vec<u32>) {
 
     a.sort();
     b.sort();
-    // println!("A: {a:?}");
-    // println!("B: {b:?}");
 
     (a, b)
 }
@@ -35,7 +30,6 @@ fn part1(a: &Vec<u32>, b: &Vec<u32>) {
             true => l - r,
             _ => r - l,
         };
-        // println!("{val}");
         total += val;
     }
     println!("Day 1 part 1: {total}");
