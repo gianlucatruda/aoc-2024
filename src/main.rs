f39bbcccb19e4eddfc652c2b61710ac454941d30
Gianluca Truda <dev@gianluca.ai>
2024-12-02 17:35:42 +0000

    Day 1a. Scrappy solution.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use std::{fs, iter::zip};
+
 fn main() {
-    println!("Hello, world!");
+    let input = fs::read_to_string("input.txt").expect("Read input");
+    let mut a: Vec<u32> = Vec::new();
+    let mut b: Vec<u32> = Vec::new();
+
+    for line in input.lines() {
+        // println!("{line}");
+        let data: Vec<&str> = line.split_whitespace().collect();
+        if data.len() < 2 {
+            continue;
+        }
+        // println!("{data:?}");
+        let left: u32 = data[0].parse().expect("str to int");
+        let right: u32 = data[1].parse().expect("str to int");
+        // println!("{left} vs {right}");
+
+        a.push(left);
+        b.push(right);
+    }
+
+    a.sort();
+    b.sort();
+    // println!("A: {a:?}");
+    // println!("B: {b:?}");
+
+    let mut total: u32 = 0;
+    for (l, r) in zip(a, b) {
+        let val = match l >= r {
+            true => l - r,
+            _ => r - l,
+        };
+        total += val;
+
+        // println!("{val}");
+    }
+    println!("Answer: {total}");
 }
