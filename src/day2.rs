da8eeb05f0f25cb5b95cdbeafa0d5948b697f010
Gianluca Truda <dev@gianluca.ai>
2024-12-03 18:41:35 +0000

    Improved day 2 code quality and fixed part 2 bug.

--- src/day2.rs
+++ src/day2.rs
@@ -1,12 +1,4 @@
 use std::fs;
-const TEST: &str = "\
-7 6 4 2 1
-1 2 7 8 9
-9 7 6 2 1
-1 3 2 4 5
-8 6 4 4 1
-1 3 6 7 9
-";
 
 fn parse_to_matrix(input: String) -> Vec<Vec<i32>> {
     let mut data: Vec<Vec<i32>> = Vec::new();
@@ -26,27 +18,26 @@ fn parse_to_matrix(input: String) -> Vec<Vec<i32>> {
 }
 
 fn is_safe(report: Vec<i32>) -> bool {
-    let mut ds: Vec<i32> = Vec::new();
-    for (i, v) in report.iter().enumerate() {
+    let dir = report[1] - report[0];
+    for (i, l) in report.iter().enumerate() {
         if i == report.len() - 1 {
-            break;
-        }
-        ds.push(report[i + 1] - v);
-    }
-    let max = ds.iter().max().unwrap();
-    let min = ds.iter().min().unwrap();
-
-    // Utter dogshit
-    if 1 <= max.abs() && max.abs() <= 3 && 1 <= min.abs() && min.abs() <= 3 {
-        // What the fuck? Why do I have to dereference here?!?!?
-        if *max < 0 && *min < 0 || *max > 0 && *min > 0 {
             return true;
         }
+        let d = report[i + 1] - l;
+        // Changes direction
+        if d * dir <= 0 {
+            break;
+        }
+        // Out out bounds
+        if d.abs() > 3 || d.abs() < 1 {
+            break;
+        }
     }
+
     false
 }
 
-fn part1(data: Vec<Vec<i32>>) -> u32 {
+fn part1(data: &Vec<Vec<i32>>) -> u32 {
     let mut sum = 0;
     for vals in data.iter() {
         if is_safe(vals.to_vec()) {
@@ -56,7 +47,7 @@ fn part1(data: Vec<Vec<i32>>) -> u32 {
     sum
 }
 
-fn part2(data: Vec<Vec<i32>>) -> u32 {
+fn part2(data: &Vec<Vec<i32>>) -> u32 {
     let mut sum = 0;
     for report in data.iter() {
         if is_safe(report.to_vec()) {
@@ -64,7 +55,7 @@ fn part2(data: Vec<Vec<i32>>) -> u32 {
             sum += 1;
             continue;
         }
-        for i in 0..5 {
+        for (i, _) in report.iter().enumerate() {
             let mut r = report.clone();
             r.remove(i);
             // println!("{report:?} | {i:?} | {r:?}");
@@ -74,25 +65,33 @@ fn part2(data: Vec<Vec<i32>>) -> u32 {
                 break;
             }
         }
-
     }
     sum
 }
 
 pub fn run() {
     let input = parse_to_matrix(fs::read_to_string("data/day2.txt").expect("Reading day2 input"));
-    let a = part1(input.clone());
+    let a = part1(&input);
     println!("Day 2 part 1: {a}");
-    let b = part2(input.clone());
+    let b = part2(&input);
     println!("Day 2 part 2: {b}");
 }
 
+const TEST: &str = "\
+7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9
+";
+
 #[test]
 fn p1() {
-    assert_eq!(part1(parse_to_matrix(TEST.to_string())), 2);
+    assert_eq!(part1(&parse_to_matrix(TEST.to_string())), 2);
 }
 
 #[test]
 fn p2() {
-    assert_eq!(part2(parse_to_matrix(TEST.to_string())), 4);
+    assert_eq!(part2(&parse_to_matrix(TEST.to_string())), 4);
 }
