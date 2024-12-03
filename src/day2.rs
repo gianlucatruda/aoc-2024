9a66d0d8b821b3703d267c6eaea7837b52d97582
Gianluca Truda <dev@gianluca.ai>
2024-12-03 17:34:35 +0000

    Modularised and tidied d2p1

--- src/day2.rs
+++ src/day2.rs
@@ -8,39 +8,40 @@ const TEST: &str = "\
 1 3 6 7 9
 ";
 
-fn part1(input: &str) -> u32 {
-    let mut sum = 0;
+fn parse_to_matrix(input: String) -> Vec<Vec<i32>> {
+    let mut data: Vec<Vec<i32>> = Vec::new();
     for line in input.lines() {
         // Account for empty lines (e.g. at EoF)
         if line.len() < 1 {
             continue;
         }
-        // println!("{line}");
         let vals: Vec<i32> = line
             .split_whitespace()
             .map(|s| s.parse().unwrap())
             .collect();
-        // println!("{vals:?}");
+        data.push(vals);
+    }
 
+    data
+}
+
+fn part1(data: Vec<Vec<i32>>) -> u32 {
+    let mut sum = 0;
+    for vals in data.iter() {
         let mut ds: Vec<i32> = Vec::new();
         for (i, v) in vals.iter().enumerate() {
             if i == vals.len() - 1 {
                 break;
             }
             ds.push(vals[i + 1] - v);
-
-            // TIL .abs and .abs_diff
         }
-        // println!("{vals:?}\t{ds:?}");
         let max = ds.iter().max().unwrap();
         let min = ds.iter().min().unwrap();
-        // println!("{max} {min}");
 
         // Utter dogshit
         if 1 <= max.abs() && max.abs() <= 3 && 1 <= min.abs() && min.abs() <= 3 {
             // What the fuck? Why do I have to dereference here?!?!?
             if *max < 0 && *min < 0 || *max > 0 && *min > 0 {
-                // println!("{line:} {min:} {max:}");
                 sum += 1;
             }
         }
@@ -48,14 +49,19 @@ fn part1(input: &str) -> u32 {
     sum
 }
 
+// fn part2(data: Vec<Vec<i32>>) -> u32 {
+//     0
+// }
+
 pub fn run() {
-    let input = fs::read_to_string("data/day2.txt").expect("Reading day2 input");
-    println!("day 2");
-    let a = part1(&input);
+    let input = parse_to_matrix(fs::read_to_string("data/day2.txt").expect("Reading day2 input"));
+    let a = part1(input);
     println!("Day 2 part 1: {a:}");
+    println!("Day 2 part 2");
+    // assert_eq!(part2(parse_to_matrix(TEST.to_string())), 4);
 }
 
 #[test]
 fn p1() {
-    assert_eq!(part1(TEST), 2);
+    assert_eq!(part1(parse_to_matrix(TEST.to_string())), 2);
 }
