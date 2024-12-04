0eff21c43654efe4978c1740bce9a47d9b3d1617
Gianluca Truda <dev@gianluca.ai>
2024-12-04 20:29:30 +0000

    More idiomatic refactor of day3part1

--- src/day3.rs
+++ src/day3.rs
@@ -4,21 +4,21 @@ fn part1(input: &str) -> i32 {
     let mut sum: i32 = 0;
     let s = input.split("mul(").collect::<Vec<&str>>();
     for hit in s.iter() {
-        let mut parts = hit.split(")").collect::<Vec<&str>>();
-        parts = parts[0].split(",").collect::<Vec<&str>>();
-        if parts.len() != 2 {
-            continue;
-        }
-        if parts[0].len() > 3 || parts[1].len() > 3 {
-            continue;
-        }
-        let left: u16 = parts[0].parse().expect("Parse left value");
-        let right: u16 = parts[1].parse().expect("Parse right value");
+        let parts = hit.split(")").collect::<Vec<&str>>();
 
-        if !(1..=999).contains(&right) || !(1..=999).contains(&left) {
+        if let Some((left, right)) = parts[0].split_once(",") {
+            if let (Ok(l), Ok(r)) = (left.parse::<u16>(), right.parse::<u16>()) {
+                if !(1..=999).contains(&r) || !(1..=999).contains(&l) {
+                    continue;
+                }
+                sum += (l as i32) * (r as i32);
+            } else {
+                continue;
+            }
+        } else {
             continue;
         }
-        sum += (left as i32) * (right as i32);
+
     }
 
     sum
