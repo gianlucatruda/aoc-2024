b4f111c8a0d32b03e6dbe3698dbcfa4253f0f8b7
Gianluca Truda <dev@gianluca.ai>
2024-12-04 19:19:08 +0000

    Tidy up day3

--- src/day3.rs
+++ src/day3.rs
@@ -1,15 +1,10 @@
 use std::fs;
 
-const _TESTCASE: &str = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
-const _TESTCASE_2: &str =
-    "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
-
 fn part1(input: &str) -> i32 {
     let mut sum: i32 = 0;
     let s = input.split("mul(").collect::<Vec<&str>>();
     for hit in s.iter() {
         let mut parts = hit.split(")").collect::<Vec<&str>>();
-        // println!("{parts:?}");
         parts = parts[0].split(",").collect::<Vec<&str>>();
         if parts.len() != 2 {
             continue;
@@ -20,7 +15,6 @@ fn part1(input: &str) -> i32 {
         let left: u16 = parts[0].parse().expect("Parse left value");
         let right: u16 = parts[1].parse().expect("Parse right value");
 
-        // println!("{parts:?}\t{left} x {right}");
         if left > 999 || left < 1 || right > 999 || right < 1 {
             continue;
         }
@@ -37,29 +31,37 @@ fn part2(input: &str) -> i32 {
     loop {
         if let Some((l, r)) = inp.split_once("don't()") {
             sum += part1(l);
-            // println!("Found DONT: {l:?} | {r:?}");
             inp = r;
-            if let Some((l, r)) = inp.split_once("do()") {
+            if let Some((_, r)) = inp.split_once("do()") {
                 inp = r;
-                // println!("Found DO: {l:?} | {r:?}");
             } else {
-                // println!("Broke inner if let");
                 return sum;
             }
         } else {
             sum += part1(inp);
-            // println!("Broke inner if let");
             return sum;
         }
     }
 }
 
 pub fn run() {
-    assert_eq!(part1(_TESTCASE), 161);
-    assert_eq!(part2(_TESTCASE_2), 48);
     let input = &fs::read_to_string("data/day3.txt").expect("Parse day3.txt");
     let p1 = part1(input);
     println!("Day 3 part 1: {p1}");
     let p2 = part2(input);
     println!("Day 3 part 2: {p2}");
+}
+
+const _TESTCASE: &str = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+const _TESTCASE_2: &str =
+    "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
+
+#[test]
+fn p1() {
+    assert_eq!(part1(_TESTCASE), 161);
+}
+
+#[test]
+fn p2() {
+    assert_eq!(part2(_TESTCASE_2), 48);
 }
