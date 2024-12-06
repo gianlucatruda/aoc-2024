268578984416dc0cefe7a0d6f1b4a3e3cfc08680
Gianluca Truda <dev@gianluca.ai>
2024-12-06 17:56:27 +0000

    Day 5 part 1 working.

--- src/day5.rs
+++ src/day5.rs
@@ -1,31 +1,49 @@
 // https://adventofcode.com/2024/day/5
 
+use std::fs;
+
 fn parse_inp(input: &str) -> (Vec<(u16, u16)>, Vec<Vec<u16>>) {
     let (rstr, ustr) = input.split_once("\n\n").expect("Parsing input");
 
-    let rules: Vec<(u16, u16)> = rstr
-        .lines()
-        .filter_map(|l| l.split_once("|"))
-        .filter_map(|(l, r)| Some((l.parse::<u16>().unwrap(), r.parse::<u16>().unwrap())))
-        .collect();
-
-    let updates: Vec<Vec<u16>> = ustr
-        .lines()
-        .filter_map(|line| {
-            if !line.is_empty() {
-                Some(line.split(',').map(|s| s.parse().unwrap()).collect())
-            } else {
-                None
-            }
-        })
-        .collect();
-
-    (rules, updates)
+    (
+        rstr.lines()
+            .filter_map(|l| l.split_once("|"))
+            .filter_map(|(l, r)| Some((l.parse::<u16>().unwrap(), r.parse::<u16>().unwrap())))
+            .collect(),
+        ustr.lines()
+            .filter_map(|line| {
+                if !line.is_empty() {
+                    Some(line.split(',').map(|s| s.parse().unwrap()).collect())
+                } else {
+                    None
+                }
+            })
+            .collect(),
+    )
 }
 
 fn part1(input: &str) -> i32 {
+    let mut sum = 0;
     let (rules, updates) = parse_inp(input);
-    0
+    for update in updates.iter() {
+        let mut violated = false;
+        for rule in rules.iter() {
+            // Check if update violates rule
+            if let Some(i_0) = update.iter().position(|&x| x == rule.0) {
+                if let Some(i_1) = update.iter().position(|&x| x == rule.1) {
+                    if i_0 > i_1 {
+                        violated = true;
+                        break;
+                    }
+                }
+            }
+        }
+        if !violated {
+            let mid = update[update.len() / 2];
+            sum += mid;
+        }
+    }
+    sum.into()
 }
 
 const _TESTCASE: &str = "\
@@ -61,6 +79,9 @@ const _TESTCASE: &str = "\
 ";
 
 pub fn run() {
-    println!("day5");
     assert_eq!(part1(_TESTCASE), 143);
+    // assert_eq!(part2(_TESTCASE), 123);
+    let input = fs::read_to_string("data/day5.txt").expect("Reading day5.txt");
+    let a = part1(&input);
+    println!("Day 5 part 1: {a}");
 }
