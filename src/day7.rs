320b2ac5595ad7e4c3b9912a79582824774827ac
Gianluca Truda <dev@gianluca.ai>
2024-12-07 17:11:19 +0000

    Day 7 part 2 (but slow)

--- src/day7.rs
+++ src/day7.rs
@@ -8,6 +8,7 @@ type Eqn = (u64, Vec<u64>);
 enum Op {
     Plus,
     Times,
+    Cat,
 }
 
 fn parse_lines(input: &str) -> Vec<Eqn> {
@@ -31,6 +32,11 @@ fn parse_lines(input: &str) -> Vec<Eqn> {
         .collect()
 }
 
+fn cat(a: u64, b: u64) -> u64 {
+    {a.to_string() + &b.to_string()}.parse().unwrap()
+
+}
+
 fn calc(parts: &[u64], perm: &[Op]) -> u64 {
     let mut total = parts[0];
     let mut i = 0;
@@ -38,17 +44,17 @@ fn calc(parts: &[u64], perm: &[Op]) -> u64 {
         match perm[i] {
             Op::Plus => total += parts[i + 1],
             Op::Times => total *= parts[i + 1],
+            Op::Cat => total = cat(total, parts[i + 1]),
         }
         i += 1;
     }
     total
 }
 
-fn valid(eq: &Eqn) -> bool {
+fn valid(eq: &Eqn, ops: Vec<Op>) -> bool {
     // println!("{eq:?}");
     let (t, parts) = eq;
-    let perms =
-        repeat_n(vec![Op::Plus, Op::Times].into_iter(), parts.len() - 1).multi_cartesian_product();
+    let perms = repeat_n(ops.into_iter(), parts.len() - 1).multi_cartesian_product();
     for perm in perms {
         // println!("{perm:?}");
         if calc(parts, &perm) == *t {
@@ -64,7 +70,19 @@ fn part1(input: &str) -> u64 {
     let equations = parse_lines(input);
     let mut sum = 0;
     for eq in equations.iter() {
-        if valid(eq) {
+        if valid(eq, vec![Op::Plus, Op::Times]) {
+            sum += eq.0;
+        }
+    }
+
+    sum
+}
+
+fn part2(input: &str) -> u64 {
+    let equations = parse_lines(input);
+    let mut sum = 0;
+    for eq in equations.iter() {
+        if valid(eq, vec![Op::Plus, Op::Times, Op::Cat]) {
             sum += eq.0;
         }
     }
@@ -85,13 +103,21 @@ const _TESTCASE: &str = "\
 ";
 
 pub fn run() {
-    println!("Day 7");
+    // assert_eq!(cat(123, 4567), 1234567);
+    // assert_eq!(cat(1, 234567), 1234567);
+    // assert_eq!(part2(_TESTCASE), 11387);
     let input = fs::read_to_string("data/day7.txt").expect("Reading day7.txt");
     let a = part1(&input);
     println!("Day 7 part 1: {a}");
+    let b = part2(&input);
+    println!("Day 7 part 2: {b}");
 }
 
 #[test]
 fn p1() {
     assert_eq!(part1(_TESTCASE), 3749);
+}
+#[test]
+fn p2() {
+    assert_eq!(part2(_TESTCASE), 11387);
 }
