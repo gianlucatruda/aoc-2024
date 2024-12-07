b22ce8b15da9fa83985570b4056eedcc26e6717a
Gianluca Truda <dev@gianluca.ai>
2024-12-07 15:00:51 +0000

    main.rs specify which day to run (with execution timer).

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::time::Instant;
+
 mod day1;
 mod day2;
 mod day3;
@@ -5,11 +7,28 @@ mod day4;
 mod day5;
 mod day6;
 
+fn run_day(d: u32) {
+    let func = match d {
+        1 => day1::run,
+        2 => day2::run,
+        3 => day3::run,
+        4 => day4::run,
+        5 => day5::run,
+        6 => day6::run,
+        _ => {
+            println!("Invalid day");
+            return;
+        }
+    };
+
+    let start = Instant::now();
+    func();
+    let duration = start.elapsed();
+    println!("{:02}.{:03}s", duration.as_secs(), duration.subsec_millis(),);
+}
+
 fn main() {
-    day1::run();
-    day2::run();
-    day3::run();
-    day4::run();
-    day5::run();
-    day6::run();
+    let args: Vec<String> = std::env::args().collect();
+    let day: u32 = args[1].parse().expect("Please provide a valid day number");
+    run_day(day);
 }
