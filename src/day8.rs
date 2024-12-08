6bf83a8ba57ef4f7f20c7b103019a89afe28a65b
Gianluca Truda <dev@gianluca.ai>
2024-12-08 08:48:38 +0000

    day 8 p1 almost working.

--- src/day8.rs
+++ src/day8.rs
@@ -1,4 +1,9 @@
-use std::{collections::HashMap, fs};
+use std::{
+    collections::{HashMap, HashSet},
+    fs,
+};
+
+use itertools::Itertools;
 
 const _TESTCASE: &str = "\
 ............
@@ -24,7 +29,7 @@ fn parse_data(input: &str) -> Vec<Vec<char>> {
         .collect()
 }
 
-fn get_coord_dic(data: Vec<Vec<char>>) -> HashMap<char, Vec<(usize, usize)>> {
+fn get_coord_dic(data: &Vec<Vec<char>>) -> HashMap<char, Vec<(usize, usize)>> {
     let mut dic: HashMap<char, Vec<(usize, usize)>> = HashMap::new();
     for (i, row) in data.iter().enumerate() {
         for (j, c) in row.iter().enumerate() {
@@ -37,15 +42,58 @@ fn get_coord_dic(data: Vec<Vec<char>>) -> HashMap<char, Vec<(usize, usize)>> {
     dic
 }
 
+fn locate_antinodes(a: (usize, usize), b: (usize, usize)) -> Vec<(usize, usize)> {
+    let dy = a.0.abs_diff(b.0);
+    let dx = a.1.abs_diff(b.1);
+    let mut res = Vec::new();
+    if a.0 as isize - dy as isize > 0 && a.1 as isize - dx as isize > 0 {
+        res.push((a.0 - dy, a.1 - dx));
+    }
+    res.push((b.0 + dy, b.1 + dx));
+
+    // vec![(a.0 - dy, a.1 - dx), (b.0 + dy, b.1 + dx)]
+    res
+}
+
 fn part1(input: &str) -> i32 {
+    let mut uniqs = HashSet::new();
     let grid = parse_data(input);
-    let coords = get_coord_dic(grid);
+    let (m, n) = (grid.len(), grid[0].len());
+    let coords = get_coord_dic(&grid);
     // println!("{coords:?}");
-    0
+    for (c, locs) in coords.iter() {
+        println!("Considering {c} ...");
+        let pairs: Vec<_> = locs.into_iter().combinations(2).collect();
+        println!("{pairs:?}");
+        for pair in pairs.iter() {
+            println!("Considering: {:?} vs {:?}", *pair[0], *pair[1]);
+            let anodes = locate_antinodes(*pair[0], *pair[1]);
+            for an in anodes.iter() {
+                if an.0 < m && an.1 < n {
+                    uniqs.insert(an.clone());
+                    println!("Antenode ({}): {:?}", *c, an);
+                }
+            }
+        }
+    }
+
+    for (i, row) in grid.iter().enumerate() {
+        for (j, c) in row.iter().enumerate() {
+            if uniqs.contains(&(i, j)) {
+                print!("#");
+            } else {
+                print!("{}", *c);
+            }
+        }
+        print!("\n");
+    }
+
+    uniqs.len().try_into().unwrap()
 }
 
 pub fn run() {
     println!("\nday 8...");
+    assert_eq!(locate_antinodes((3, 4), (5, 5)), vec![(1, 3), (7, 6)]);
     assert_eq!(part1(_TESTCASE), 14);
     let input = fs::read_to_string("data/day8.txt").expect("Read day8.txt");
     let a = part1(&input);
