70da08afc9bba8d7f0674b05bf508858e34381a9
Gianluca Truda <dev@gianluca.ai>
2024-12-11 16:07:53 +0000

    Day 11 part 1

--- src/day11.rs
+++ src/day11.rs
@@ -1,5 +1,7 @@
+use std::fs;
+
 fn part1(input: &str, blinks: u32) -> i32 {
-    let mut stones: Vec<u32> = input
+    let mut stones: Vec<u64> = input
         .trim()
         .split(' ')
         .map(|s| s.parse().unwrap())
@@ -7,7 +9,29 @@ fn part1(input: &str, blinks: u32) -> i32 {
 
     println!("Stones: {stones:?}");
 
-    0
+    for _ in 0..blinks {
+        let mut new: Vec<u64> = Vec::new();
+        for s in stones {
+            let digits = (s as f64).log10() as u32 + 1;
+            if digits % 2 == 0 {
+                let s_str = s.to_string();
+                let left_str: String = s_str.chars().take((digits / 2) as usize).collect();
+                let left: u64 = left_str.parse().unwrap();
+                let right_str: String = s_str.chars().rev().take((digits / 2) as usize).collect();
+                let right: u64 = right_str.chars().rev().collect::<String>().parse().unwrap();
+                new.push(left);
+                new.push(right);
+            } else {
+                match s {
+                    0 => new.push(1),
+                    _ => new.push(s * 2024),
+                }
+            }
+        }
+        stones = new;
+        // println!("Update: {stones:?}");
+    }
+    stones.len().try_into().unwrap()
 }
 
 const _EXAMPLE: &str = "0 1 10 99 999";
@@ -18,4 +42,6 @@ pub fn run() {
     assert_eq!(part1(_EXAMPLE, 1), 7);
     assert_eq!(part1(_TESTCASE, 6), 22);
     assert_eq!(part1(_TESTCASE, 25), 55312);
+    let input = fs::read_to_string("data/day11.txt").expect("Reading day11.txt");
+    println!("Day 11 part 1: {}", part1(&input, 25));
 }
